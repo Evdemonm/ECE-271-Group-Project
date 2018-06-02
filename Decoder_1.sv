3ab691804c1e1dfc58bd4357fdad0a9b9512f076
austin <wilmotha@oregonstate.edu>
2018-06-01 17:14:54 -0700

    added files that shuman gave us

--- Decoder_1.sv
+++ Decoder_1.sv
@@ -1,4 +1,4 @@
 module decoder_1(input  logic[12:0] nes,
 				 input  logic[3:0] state,
 				 output logic nes_decoded);
-	
+endmodule
--- Mux.sv
+++ Mux.sv
@@ -5,12 +5,12 @@ module mux1(input logic [4:0]nes_in,
 	always_comb
 	begin
 	if(state == 1 && (nes_in < 4)) 
-				assign nes_out = nes_in; 
+			nes_out = nes_in; 
 	else if(state == 2 && (nes_in > 3 && nes_in < 8))
-			assign nes_out = nes_in;
+			nes_out = nes_in;
 	else if(state == 3 && (nes_in > 7 && nes_in < 12))
-			assign nes_out = nes_in;
+			nes_out = nes_in;
 	else
-			assign nes_out = 12;
+			nes_out = 12;
 	end
 endmodule
