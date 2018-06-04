bb5d5a0c29abc7c64da8b3cf2072d9aa9ad9f0ca
austin <wilmotha@oregonstate.edu>
2018-06-03 20:11:58 -0700

    the IR decoder might work!!!

--- Shuman Files/PS2Keyboard/find_data.py
+++ Shuman Files/PS2Keyboard/find_data.py
@@ -10,18 +10,12 @@ with open(sys.argv[1], newline = '') as file:
     check = 0
     for idx,line in enumerate(a):
         if idx > 14:
-            # try:
             if(line[2] != '' or line[0] !=''):
                 t=float(line[2])
                 time = float(line[0])
-            # except Exception as e:
-            # else:
-            #     # print("value: ", line[2])
-            #     # print("time", line[0])
             timeAdd = 0
             timeAdd2 = 0
             if idx > 14 and t < 1 and check == 0:
-                # print("falling - time:", time, "\n");
                 check = 1
                 timefall = time
             if idx > 14 and t > 4 and check == 1:
@@ -29,3 +23,4 @@ with open(sys.argv[1], newline = '') as file:
                 print("Time:", timerise, "\n");
                 check = 0
                 timeAdd2 = time
+    # file = open("ps/2Data.txt", "w")
