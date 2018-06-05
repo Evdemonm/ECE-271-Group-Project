cc349564f3afad26836141e7c2cdbd9e4035bd91
austin <wilmotha@oregonstate.edu>
2018-06-05 16:42:39 -0700

    PS/2 Keyboard decoding is maybe done just need to test

--- Shuman Files/PS2Keyboard/find_data.py
+++ Shuman Files/PS2Keyboard/find_data.py
@@ -10,8 +10,8 @@ with open(sys.argv[1], newline = '') as file:
     check = 0
     for idx,line in enumerate(a):
         if idx > 14:
-            if(line[2] != '' or line[0] !=''):
-                t=float(line[2])
+            if(line[2] != '' or line[0] !='' or line[1] != ''):
+                t=float(line[1])
                 time = float(line[0])
             timeAdd = 0
             timeAdd2 = 0
@@ -20,7 +20,22 @@ with open(sys.argv[1], newline = '') as file:
                 timefall = time
             if idx > 14 and t > 4 and check == 1:
                 timerise = time - timefall
-                print("Time:", timerise, "\n");
+                print("Time:", timerise)
+                print("Value:", line[2])
+                if(float(line[2]) > 4):
+                    print("bit:", 1, "\n")
+                    data.append(1)
+                elif(float(line[2]) < 1):
+                    print("bit:", 0, "\n")
+                    data.append(0)
                 check = 0
                 timeAdd2 = time
+    if(sys.argv[2] == "-w"):
+        fileR = open("PS2Data.txt", "r")
+        dataPrevious = fileR.read()
+        fileR.close()
+        file = open("PS2Data.txt", "w")
+        dataWrite = dataPrevious + str(sys.argv[1]) + ": " + str(data) +"\n"
+        file.write(dataWrite);
+    print("Data:", data)
     # file = open("ps/2Data.txt", "w")
