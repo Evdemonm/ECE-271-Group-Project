364e682d75df9bd45ff318bcce90276c20199986
Evdemonm <32214874+Evdemonm@users.noreply.github.com>
2018-06-06 17:51:56 -0700

    For the State Machine

---
+++ shiftRegisterStateMachine.sv
@@ -0,0 +1,15 @@
+module shiftreg(input  logic clk,
+				input  logic reset, 
+				input  logic serial_data_in,
+				output logic [31:0] parallel_data_out);
+
+always @ ( posedge clk or negedge reset)
+	begin
+	  if (!reset) 
+		begin
+			parallel_data_out <= 32'b0;
+		end
+	  else  
+		 parallel_data_out[31:0] <= {parallel_data_out[30:0],serial_data_in};
+	end
+endmodule
