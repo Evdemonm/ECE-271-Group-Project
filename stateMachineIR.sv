94be358aceecbc75ecd67a376154e28362b83cc9
ItsMePND <pndNicholas@gmail.com>
2018-06-06 23:29:25 -0700

    FIX

--- stateMachineIR.sv
+++ stateMachineIR.sv
@@ -8,11 +8,52 @@ module statemachine(
  );
        
  
+ enum{
    IDLE = 32'b00,
    READ = 32'b01,
    END = 32'b10,
    NA = 32'b11
  } sm_ps, sm_ns;
+
+ always_ff @ (posedge clk, negedge reset)
+      begin
+		 if(~reset)
+            sm_ps <= IDLE;
+         else
+            sm_ps <= sm_ns;
+	  end
+
+always_comb
+         begin	
+			 
+		 case(sm_ps)
+	
+			 IDLE:  
+			     if(read == 1)
+					 sm_ns = READ;
+			     else
+					 sm_ns = IDLE;
+					 
+			 READ:      
+                  if(DONE == 1)
+                     sm_ns = END;
+			      else if(ERROR == 1)
+					 sm_ns = IDLE;
+                  else					 
+			         sm_ns = READ; 
+		     END:
+			     sm_ns = IDLE;
+			 
+			 NA:
+			    sm_ns = IDLE;
+				
+			 default:
+                sm_ns = IDLE;			 
+
+			 endcase	     
+	      end
+ endmodule
+
 /*
 logic [1:0] currentstate;
 
@@ -55,41 +96,3 @@ always_comb
 	      end
  endmodule
 */
- always_ff @ (posedge clk, negedge reset)
-      begin
-		 if(~reset)
-            sm_ps <= IDLE;
-         else
-            sm_ps <= sm_ns;
-	  end
-
-always_comb
-         begin	
-			 
-		 case(sm_ps)
-	
-			 IDLE:  
-			     if(read == 1)
-					 sm_ns = READ;
-			     else
-					 sm_ns = IDLE;
-					 
-			 READ:      
-                  if(DONE == 1)
-                     sm_ns = END;
-			      else if(ERROR == 1)
-					 sm_ns = IDLE;
-                  else					 
-			         sm_ns = READ; 
-		     END:
-			     sm_ns = IDLE;
-			 
-			 NA:
-			    sm_ns = IDLE;
-				
-			 default:
-                sm_ns = IDLE;			 
-
-			 endcase	     
-	      end
- endmodule
