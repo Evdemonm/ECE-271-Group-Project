9d419549bea3f2741af262568a738d5e9f9c520a
Evdemonm <32214874+Evdemonm@users.noreply.github.com>
2018-06-08 00:56:37 -0700

    IR Folder and buttonState machine

---
+++ coolFeature/buttonState.sv
@@ -0,0 +1,19 @@
+module buttonState(            input slowCLK,
+            output [2:0] State );
+
+      logic [2:0] ST = 2;
+      
+			
+ always_ff @ (posedge slowCLK)
+      begin
+		 if(ST == 2)
+            ST <= 0;
+		 else if(ST == 0)
+			 ST <= 1;
+         else
+            ST <= 2;
+	  end
+
+     assign State = ST; 
+	 
+  endmodule
